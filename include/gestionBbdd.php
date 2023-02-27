d272deb2554dd6c5f0a556145f0cf2b2dd9132dc
fragomdia <fmgomezd02@educarex.es>
2023-02-27 19:08:55 +0100

    add_diego

--- include/gestionBbdd.php
+++ include/gestionBbdd.php
@@ -18,10 +18,13 @@ class GestionBBDD {
 
     public static function productos($inicio, $longitud) {
         try {
-            $sql="select * from productos limit $inicio, $longitud";
-            //$sql="select * from productos limit :n_inicio, :n_longitud;";
+            //$sql="select * from productos limit $inicio, $longitud";
+            $sql="select * from productos limit :n_inicio, :n_longitud;";
             $conexion=self::realizarConexion();
-		    $resultado=$conexion->query($sql);
+		    $resultado=$conexion->prepare($sql);
+            $resultado->bindParam(':n_inicio', $inicio, PDO::PARAM_INT);
+            $resultado->bindParam(':n_longitud', $longitud, PDO::PARAM_INT);
+            $resultado->execute();
             //$resultado->execute(array(":n_inicio"=>$inicio, ":n_longitud"=>$longitud));
 	        $arra_productos=array();
             while ($fila=$resultado->fetch()){
